984c59a9292ac52ce9bd91fce20ec26533e49b20
ryutah <ryuta0221@gmail.com>
2018-12-28 14:18:01 +0900

    chore(script): updates hie install script

--- script/install_lsp.sh
+++ script/install_lsp.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-hie_version=8.4.4
+hie_version="8.4.4 8.6.3"
 
 cd $HOME/.local/nvim
 
 git clone https://github.com/haskell/haskell-ide-engine --recursive
 cd haskell-ide-engine
 
-make hie-${hie_version}
-stack --stack-yaml stack-${hie_version}.yaml exec hoogle generate
+echo ${hie_version} | xargs -n 1 -I VERSION make hie-VERSION
+echo ${hie_version} | xargs -n 1 -I VERSION stack --stack-yaml stack-VERSION.yaml exec hoogle generate

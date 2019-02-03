4a5af923c56de283b3f19fa045a5bc425475ece3
ryutah <ryuta0221@gmail.com>
2019-02-03 15:10:25 +0900

    feat(lsp): adds go lsp configuration(wip)

--- autoload/hook/source/neosnippet.vim
+++ autoload/hook/source/neosnippet.vim
@@ -1,5 +1,6 @@
 function! hook#source#neosnippet#load() abort
   let g:neosnippet#snippets_directory = expand(g:neovim_home) . '/snippets'
+  " let g:neosnippet#enable_complete_done = 1 " Not run well....
 
   imap <C-k> <Plug>(neosnippet_expand_or_jump)
   smap <C-k> <Plug>(neosnippet_expand_or_jump)
--- script/install_lsp.sh
+++ script/install_lsp.sh
@@ -5,9 +5,15 @@ set -e
 hie_version="8.4.4 8.6.3"
 
 cd $HOME/.local/nvim
+wd=`pwd`
 
 git clone https://github.com/haskell/haskell-ide-engine --recursive
 cd haskell-ide-engine
 
 echo ${hie_version} | xargs -n 1 -I VERSION make hie-VERSION
 echo ${hie_version} | xargs -n 1 -I VERSION stack --stack-yaml stack-VERSION.yaml exec hoogle generate
+
+cd ${pwd}
+git clone https://github.com/saibing/bingo.git
+cd bingo
+GO111MODULE=on go install

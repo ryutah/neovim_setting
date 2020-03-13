bc448992c5a21c9e6f72e202330079055c41e3a4
ryutah <ryuta0221@gmail.com>
2020-03-13 21:15:02 +0900

    fix(script): fix required tool install scripts

--- script/install_tools.sh
+++ script/install_tools.sh
@@ -1,4 +1,60 @@
 #!/bin/sh
+nvim_lib_dir=$HOME/.local/nvim
+
 set -eux
 
+###############################################
+# Go
+###############################################
 go get -u golang.org/x/lint/golint honnef.co/go/tools/cmd/staticcheck golang.org/x/tools/...
+
+###############################################
+# JavaScript, TypeScript
+###############################################
+npm install -g \
+  eslint \
+  tslint \
+  prettier \
+  fixjson \
+  typescript
+
+###############################################
+# Python
+###############################################
+pip install -U \
+  python-language-server \
+  autopep8 \
+  flake8
+
+###############################################
+# Ruby
+###############################################
+gem install rubocop
+
+###############################################
+# Java
+###############################################
+google_java_format_version=1.7
+wget -O ${nvim_lib_dir}/google-java-format.jar \
+  https://github.com/google/google-java-format/releases/download/google-java-format-${google_java_format_version}/google-java-format-${google_java_format_version}-all-deps.jar
+rm -rf ${nvim_lib_dir}/google_java_format
+echo '#!/bin/sh\njava -jar /Users/ryuta/.local/nvim/google-java-format.jar --aosp $*' > ${nvim_lib_dir}/google_java_format
+chmod +x ${nvim_lib_dir}/google_java_format
+
+# TODO: Eclipse LSP JDT Install
+
+
+###############################################
+# SQL
+###############################################
+# go get github.com/jackc/sqlfmt/...
+
+###############################################
+# OCaml
+###############################################
+npm install -g ocaml-language-server
+
+###############################################
+# Markdown
+###############################################
+npm install -g markdownlint-cli

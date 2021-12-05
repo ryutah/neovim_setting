2b1c400cdc0a8e92d1803959cfe45ad69d1d4b99
ryutah <6662577+ryutah@users.noreply.github.com>
2021-12-05 18:40:52 +0900

    refactor(script): define path as variable

--- script/install.sh
+++ script/install.sh
@@ -3,11 +3,16 @@
 set -eu
 
 cd $(dirname $0)/..
-wd=$(pwd)
 
-mkdir -p ${HOME}/.local/bin
+# Path informations
+wd=$(pwd)
+bin_path=${HOME}/.local/bin
+config_path=${XDG_CONFIG_HOME:-$HOME/.config}
+
+mkdir -p ${bin_path}
+
 if [[ ! -d ${HOME}/.config/nvim ]]; then
-  cd ${HOME}/.config && ln -sf ${wd}/ nvim
+  cd ${config_path} && ln -sf ${wd}/ nvim
 fi
 cd ${HOME} && ln -sf ${wd}/.vimspector.json
 
@@ -16,7 +21,7 @@ cd ${wd}
 source ./script/versions.sh
 
 # Make simlink to extends scripts
-ls ./script/extends | xargs -tI {} ln -f -s $(pwd)/script/extends/{} ${HOME}/.local/bin/
+ls ./script/extends | xargs -tI {} ln -f -s $(pwd)/script/extends/{} ${bin_path}/
 
 # Install neovim extends
 pip install -U pynvim autopep8
@@ -33,13 +38,12 @@ go install github.com/mdempsky/gocode@latest
 go install github.com/x-motemen/gore/cmd/gore@latest
 
 # Install Node tools
-mkdir -p ${HOME}/.local/bin
 npx npm-check-updates -u
 npm install
-ls ./node_modules/.bin | xargs -tI {} ln -f -s $(pwd)/node_modules/.bin/{} ${HOME}/.local/bin/
+ls ./node_modules/.bin | xargs -tI {} ln -f -s $(pwd)/node_modules/.bin/{} ${bin_path}/
 
 # create symlink of snippets
-coc_config_path=${XDG_CONFIG_HOME:-$HOME/.config}/coc
+coc_config_path=${config_path}/coc
 mkdir -p ${coc_config_path}
 ln -f -s $(pwd)/snippets/ultisnips ${coc_config_path}/
 
@@ -49,12 +53,12 @@ mkdir -p tmp && cd tmp
 clang_llvm_file="clang+llvm-${CLANG_LLVM_VERSION}-x86_64-linux-gnu-ubuntu-${OS_VERSION}"
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${CLANG_LLVM_VERSION}/${clang_llvm_file}.tar.xz
 tar xvf ${clang_llvm_file}.tar.xz
-mv ${clang_llvm_file}/bin/clang-format $HOME/.local/bin/
+mv ${clang_llvm_file}/bin/clang-format ${bin_path}/
 
 # for terraform lsp
 terraform_lsp_file="terraform-ls_${TERRAFORM_LSP_VERSION}_linux_amd64"
 wget https://releases.hashicorp.com/terraform-ls/${TERRAFORM_LSP_VERSION}/${terraform_lsp_file}.zip
 unzip ${terraform_lsp_file}.zip
-mv terraform-ls $HOME/.local/bin/
+mv terraform-ls ${bin_path}/
 
 cd .. && rm -rf tmp

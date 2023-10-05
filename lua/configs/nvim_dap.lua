ea7cc3cccde50cdf7534d473dcc3f4f2b9ef8dad
ryutah <6662577+ryutah@users.noreply.github.com>
2023-10-05 10:06:17 +0900

    chore: adds dap repl toggle keymap

--- lua/configs/nvim_dap.lua
+++ lua/configs/nvim_dap.lua
@@ -48,12 +48,17 @@ dap.adapters.delve = {
 require("nvim-dap-virtual-text").setup()
 require('nvim-dap-repl-highlights').setup()
 
+local function repl_toggle()
+  dap.repl.toggle({ height = 10 })
+end
+
 -- default keymaps
 vim.keymap.set('n', '[nvim_dap]', '<Nop>')
 vim.keymap.set('n', '<Leader>d', '[nvim_dap]', { remap = true })
 vim.keymap.set('n', '[nvim_dap]b', dap.toggle_breakpoint, { silent = true, desc = 'dap toggle breakpoint' })
 vim.keymap.set('n', '[nvim_dap]c', dap.continue, { silent = true, desc = 'dap continue' })
 vim.keymap.set('n', '[nvim_dap]t', dap_go.debug_test, { silent = true, desc = 'dap_go debug test' })
+vim.keymap.set('n', '[nvim_dap]r', repl_toggle, { silent = true, desc = 'dap toggle repl' })
 
 vim.api.nvim_create_augroup('nvim_dap_filetype_keymap', {})
 vim.api.nvim_create_autocmd('FileType', {

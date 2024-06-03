6e0e5aafdd6e02c4496a554a541db358f031cc45
ryutah <6662577+ryutah@users.noreply.github.com>
2024-06-03 23:05:04 +0900

    feat: adds background color auto change

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,9 +1,15 @@
-vim.opt.background = "dark"
+local windows = require('utils/windows')
 
 if vim.fn.has('termguicolors') then
   vim.cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
   vim.cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
   vim.opt.termguicolors = true
+end
+
+if windows().is_wsl2() and windows().is_apps_use_light_theme() then
+  vim.opt.background = "light"
+else
+  vim.opt.background = "dark"
 end
 
 vim.g.everforest_background = 'hard'
--- lua/configs/vim_airline.lua
+++ lua/configs/vim_airline.lua
@@ -1,1 +1,7 @@
-vim.g.airline_theme = 'ayu_dark'
+local windows = require('utils/windows')
+
+if windows().is_wsl2() and windows().is_apps_use_light_theme() then
+  vim.g.airline_theme = 'ayu_light'
+else
+  vim.g.airline_theme = 'ayu_dark'
+end
